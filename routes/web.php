9612c38ac7e4aaba840ec52078bb5674e69b17df
Muhammad Aqilul Muttaqin <aqilulmuttaqin16@gmail.com>
2023-06-07 18:15:09 +0700

    Tampilan Admin

--- routes/web.php
+++ routes/web.php
@@ -24,27 +24,28 @@ use Illuminate\Support\Facades\Auth;
 */
 
 Auth::routes();
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('logout', [LoginController::class, 'logout']);
 Route::middleware(['auth', 'ceklevel:admin'])->group(function(){
+    Route::get('/', [HomeController::class, 'indexAdmin']);
+    Route::get('/home', [HomeController::class, 'indexAdmin']);
     Route::get('/admin', [BookingController::class, 'tampil']);
 });
 Route::middleware(['auth', 'ceklevel:user'])->group(function(){
-    Route::get('/booking', [BookingController::class, 'index']);
-    Route::get('/booking', [BookingController::class, 'create'])->name('booking.create');
-    Route::post('/booking', [BookingController::class, 'store'])->name('booking.store');
-    Route::get('/booking/{booking_id}/payment', [BookingController::class, 'payment'])->name('booking.payment');
-    Route::post('/booking/{booking_id}/payment/process', [BookingController::class, 'processPayment'])->name('booking.processPayment');
-<<<<<<< HEAD
-    Route::get('/history', [BookingController::class,'getHistory'])->name('booking.history');
-=======
->>>>>>> 2220780c7b6caf36349f9948dbefd8870ada793c
-});
-Route::middleware(['auth', 'ceklevel:admin,user'])->group(function(){
     Route::get('/', [HomeController::class, 'index']);
     Route::get('/information', [InformationController::class, 'index']);
     Route::get('/gallery', [GalleryController::class, 'index']);
     Route::get('/flow', [FlowController::class, 'index']);
     Route::get('/cek-kuota', [KuotaController::class, 'cekKuota'])->name('cek-kuota');
     Route::get('/about-us', [AboutController::class, 'index']);
+    Route::get('/booking', [BookingController::class, 'index']);
+    Route::get('/booking', [BookingController::class, 'create'])->name('booking.create');
+    Route::post('/booking', [BookingController::class, 'store'])->name('booking.store');
+    Route::get('/booking/{booking_id}/payment', [BookingController::class, 'payment'])->name('booking.payment');
+    Route::post('/booking/{booking_id}/payment/process', [BookingController::class, 'processPayment'])->name('booking.processPayment');
+    Route::get('/history', [BookingController::class,'getHistory'])->name('booking.history');
+});
+Route::middleware(['auth', 'ceklevel:admin'])->group(function(){
+    Route::get('/', [HomeController::class, 'indexAdmin']);
+    Route::get('/home', [HomeController::class, 'indexAdmin']);
+    Route::get('/admin', [BookingController::class, 'tampil']);
 });
